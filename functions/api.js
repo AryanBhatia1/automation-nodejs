de89d500d116b87df8040653368910136b1851aa
AryanBhatia1 <80618333+AryanBhatia1@users.noreply.github.com>
2022-10-28 19:42:41 +0530

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
 const webdriver = require("selenium-webdriver");
 const chrome = require("chromedriver");
 
